51c9fb790f95ce93b58777ede695b2a5cecd9247
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2016-02-12 22:01:00 -0200

    Update setup script for new stable version.

--- aux/setup.sh
+++ aux/setup.sh
@@ -38,15 +38,15 @@ if [ "$answerStable" == "y" ]; then
 
   cd ..
  
-  git checkout 1.4.x
+  git checkout 1.5.x
    
   cd src/fe
    
-  git checkout 1.4.0
+  git checkout 1.5.0
 
   cd ../../aux
 
-  echo "Changed to latest stable version: 1.4.x"
+  echo "Changed to latest stable version: 1.5.x"
 
 fi
 
