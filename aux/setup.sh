fa4c6e58c78c2898e3c9f05902b7d024695178b7
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2019-11-16 17:59:21 -0300

    Updated setup script for stable version.

--- aux/setup.sh
+++ aux/setup.sh
@@ -36,19 +36,19 @@ fi
 
 if [ "$answerStable" == "y" ]; then
 
-  git checkout 2.2.x
+  git checkout 2.3.x
 
   if [ "$answerFrontEnd" == "y" ]; then
 
     cd ../src/fe
 
-    git checkout 2.2.x
+    git checkout 2.3.x
 
     cd ../../aux
 
   fi
 
-  echo "Changed to latest stable version: 2.2.x"
+  echo "Changed to latest stable version: 2.3.x"
 
 fi
 
