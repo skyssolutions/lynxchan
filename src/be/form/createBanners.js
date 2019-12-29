171579b3ca74a84963a93a044c75cf627082f008
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2019-12-29 14:33:41 -0300

    Fixed final functions that used file uploading.

--- src/be/form/createBanners.js
+++ src/be/form/createBanners.js
@@ -31,8 +31,22 @@ exports.process = function(req, res) {
 
   formOps.getAuthenticatedPost(req, res, true, function gotData(auth, userData,
       parameters) {
-    exports.createBanners(parameters, userData, res, auth, req.language, formOps
-        .json(req));
+
+    // style exception, too simple
+    formOps.validateMimes(parameters, parameters.files, function(error) {
+
+      var json = formOps.json(req);
+
+      if (error) {
+        formOps.outputError(error, 500, res, req.language, json, auth);
+      } else {
+        exports.createBanners(parameters, userData, res, auth, req.language,
+            json);
+      }
+
+    });
+    // style exception, too simple
+
   });
 
 };
