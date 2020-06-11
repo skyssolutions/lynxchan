cbf203f2ed6ea9c31396283fc5fcbf054d314081
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2020-06-11 17:04:55 -0300

    Implemented setting that disabled captcha for bans entirely.

--- src/be/form/account.js
+++ src/be/form/account.js
@@ -9,33 +9,33 @@ exports.process = function(req, res) {
   formOps.getAuthenticatedPost(req, res, false,
       function gotData(auth, userData) {
 
-        if (formOps.json(req)) {
-
-          var settings = require('../settingsHandler').getGeneralSettings();
-
-          var creationRequirement = settings.boardCreationRequirement;
-
-          var allowed = userData.globalRole <= creationRequirement;
-
-          allowed = allowed || creationRequirement > miscOps.getMaxStaffRole();
-
-          formOps.outputResponse('ok', {
-            login : userData.login,
-            email : userData.email || '',
-            ownedBoards : userData.ownedBoards || [],
-            settings : userData.settings || [],
-            volunteeredBoards : userData.volunteeredBoards || [],
-            disabledLatestPostings : settings.disableLatestPostings || false,
-            boardCreationAllowed : allowed,
-            globalRole : isNaN(userData.globalRole) ? 4 : userData.globalRole
-          }, res, null, auth, null, true);
-
-        } else {
-
+        if (!formOps.json(req)) {
           res.writeHead(200, miscOps.getHeader('text/html', auth));
           res.end(domManipulator.account(userData, req.language));
-
+          return;
         }
+
+        var settings = require('../settingsHandler').getGeneralSettings();
+
+        var creationRequirement = settings.boardCreationRequirement;
+
+        var allowed = userData.globalRole <= creationRequirement;
+
+        allowed = allowed || creationRequirement > miscOps.getMaxStaffRole();
+
+        formOps.outputResponse('ok',
+            {
+              noCaptchaBan : require('../settingsHandler')
+                  .getGeneralSettings().disableBanCaptcha,
+              login : userData.login,
+              email : userData.email || '',
+              ownedBoards : userData.ownedBoards || [],
+              settings : userData.settings || [],
+              volunteeredBoards : userData.volunteeredBoards || [],
+              disabledLatestPostings : settings.disableLatestPostings || false,
+              boardCreationAllowed : allowed,
+              globalRole : isNaN(userData.globalRole) ? 4 : userData.globalRole
+            }, res, null, auth, null, true);
 
       }, false, true);
 };
