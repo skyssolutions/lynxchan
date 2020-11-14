a7e87e68b8ba28260148ce0387e951d018079160
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2020-11-14 17:13:17 -0300

    Implemented rule editing.

--- src/be/engine/boardOps/ruleOps.js
+++ src/be/engine/boardOps/ruleOps.js
@@ -41,41 +41,39 @@ exports.addBoardRule = function(parameters, userData, language, callback) {
     boardUri : parameters.boardUri
   }, function gotBoard(error, board) {
     if (error) {
-      callback(error);
+      return callback(error);
     } else if (!board) {
-      callback(lang(language).errBoardNotFound);
+      return callback(lang(language).errBoardNotFound);
     } else if (userData.login !== board.owner && !globallyAllowed) {
-      callback(!lang(language).errDeniedRuleManagement);
-    } else {
-      if (board.rules && board.rules.length >= maxRulesCount) {
-        callback(lang(language).errRuleLimitReached);
-
-        return;
-      }
-
-      miscOps.sanitizeStrings(parameters, newRuleParameters);
-
-      // style exception, too simple
-      boards.updateOne({
-        boardUri : parameters.boardUri
-      }, {
-        $push : {
-          rules : parameters.rule
-        }
-      }, function updatedRules(error) {
-        if (error) {
-          callback(error);
-        } else {
-          process.send({
-            board : board.boardUri,
-            rules : true
-          });
-          callback();
-        }
-      });
-      // style exception, too simple
-
+      return callback(!lang(language).errDeniedRuleManagement);
     }
+
+    if (board.rules && board.rules.length >= maxRulesCount) {
+      return callback(lang(language).errRuleLimitReached);
+    }
+
+    miscOps.sanitizeStrings(parameters, newRuleParameters);
+
+    // style exception, too simple
+    boards.updateOne({
+      boardUri : parameters.boardUri
+    }, {
+      $push : {
+        rules : parameters.rule
+      }
+    }, function updatedRules(error) {
+      if (error) {
+        callback(error);
+      } else {
+        process.send({
+          board : board.boardUri,
+          rules : true
+        });
+        callback();
+      }
+    });
+    // style exception, too simple
+
   });
 };
 
@@ -89,48 +87,45 @@ exports.deleteRule = function(parameters, userData, language, callback) {
     boardUri : parameters.boardUri
   }, function gotBoard(error, board) {
     if (error) {
-      callback(error);
+      return callback(error);
     } else if (!board) {
-      callback(lang(language).errNoBoardFound);
+      return callback(lang(language).errNoBoardFound);
     } else if (board.owner !== userData.login && !globallyAllowed) {
-      callback(lang(language).errDeniedRuleManagement);
-    } else {
-
-      if (isNaN(parameters.ruleIndex)) {
-        callback(lang(language).errInvalidIndex);
-        return;
-      }
-
-      var index = +parameters.ruleIndex;
-
-      if (!board.rules || board.rules.length <= index) {
-        callback();
-        return;
-      }
-
-      board.rules.splice(index, 1);
-
-      // style exception, too simple
-      boards.updateOne({
-        boardUri : parameters.boardUri
-      }, {
-        $set : {
-          rules : board.rules
-        }
-      }, function updatedRules(error) {
-        if (error) {
-          callback(error);
-        } else {
-          process.send({
-            board : board.boardUri,
-            rules : true
-          });
-          callback();
-        }
-      });
-      // style exception, too simple
-
+      return callback(lang(language).errDeniedRuleManagement);
     }
+
+    if (isNaN(parameters.ruleIndex)) {
+      return callback(lang(language).errInvalidIndex);
+    }
+
+    var index = +parameters.ruleIndex;
+
+    if (!board.rules || board.rules.length <= index) {
+      return callback();
+    }
+
+    board.rules.splice(index, 1);
+
+    // style exception, too simple
+    boards.updateOne({
+      boardUri : parameters.boardUri
+    }, {
+      $set : {
+        rules : board.rules
+      }
+    }, function updatedRules(error) {
+      if (error) {
+        callback(error);
+      } else {
+        process.send({
+          board : board.boardUri,
+          rules : true
+        });
+        callback();
+      }
+    });
+    // style exception, too simple
+
   });
 };
 
@@ -161,4 +156,59 @@ exports.boardRules = function(boardUri, userData, language, callback) {
       callback(null, board.rules || []);
     }
   });
+};
+
+exports.editRule = function(parameters, userData, language, callback) {
+
+  var globallyAllowed = userData.globalRole <= 1 && globalBoardModeration;
+
+  parameters.boardUri = parameters.boardUri.toString();
+
+  boards.findOne({
+    boardUri : parameters.boardUri
+  }, function gotBoard(error, board) {
+    if (error) {
+      return callback(error);
+    } else if (!board) {
+      return callback(lang(language).errNoBoardFound);
+    } else if (board.owner !== userData.login && !globallyAllowed) {
+      return callback(lang(language).errDeniedRuleManagement);
+    }
+
+    if (isNaN(parameters.ruleIndex)) {
+      return callback(lang(language).errInvalidIndex);
+    }
+
+    miscOps.sanitizeStrings(parameters, newRuleParameters);
+
+    var index = +parameters.ruleIndex;
+
+    if (!parameters.rule || !board.rules || board.rules.length <= index) {
+      return callback();
+    }
+
+    board.rules[index] = parameters.rule;
+
+    // style exception, too simple
+    boards.updateOne({
+      boardUri : parameters.boardUri
+    }, {
+      $set : {
+        rules : board.rules
+      }
+    }, function updatedRules(error) {
+      if (error) {
+        callback(error);
+      } else {
+        process.send({
+          board : board.boardUri,
+          rules : true
+        });
+        callback();
+      }
+    });
+    // style exception, too simple
+
+  });
+
 };
