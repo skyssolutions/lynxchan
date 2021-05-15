06bfc1563f6e849872df92385046eb45129f48c1
Sérgio Augusto Vianna <sergio.a.vianna@gmail.com>
2021-05-15 14:03:32 -0300

    Updated setup script for stable version.

--- aux/setup.sh
+++ aux/setup.sh
@@ -36,19 +36,19 @@ fi
 
 if [ "$answerStable" == "y" ]; then
 
-  git checkout 2.5.x
+  git checkout 2.6.x
 
   if [ "$answerFrontEnd" == "y" ]; then
 
     cd ../src/fe
 
-    git checkout 2.5.x
+    git checkout 2.6.x
 
     cd ../../aux
 
   fi
 
-  echo "Changed to latest stable version: 2.5.x"
+  echo "Changed to latest stable version: 2.6.x"
 
 fi
 
